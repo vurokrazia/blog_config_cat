39a300ec6d946fc152c3e3eeca5492d6624236fe
jesus martinez <jesus.alberto.vk@gmail.com>
2021-08-26 19:01:42 -0500

    first commit

--- config/initializers/config_kat.rb
+++ config/initializers/config_kat.rb
@@ -3,7 +3,7 @@ require "configcat"
 class ConfigKat
   def self.config_cat_client
     @config_cat_client ||= ConfigCat.create_client_with_auto_poll(
-      "6t3YCCuQ_UKD6OqSfMfGMQ/NEtWRxZ0NUu_pb_Zctmvbg",
+      ENV['ConfigKatToken'],
       poll_interval_seconds: 5,
     )
   end
